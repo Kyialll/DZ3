b1120803c0d0905bf37f3759f9788177eb57743d
Kyialll <119960477+Kyialll@users.noreply.github.com>
2022-12-13 18:50:35 +0600

    Add files via upload

---
+++ DZ3_Kyial_21-03_JS/programm_1/app.js
@@ -0,0 +1,5 @@
+var num=0
+while (num<20){
+    num++;
+    if (num%2==0)console.log(num)
+}
---
+++ DZ3_Kyial_21-03_JS/programm_2/app.js
@@ -0,0 +1,7 @@
+var num=0;
+var sum=0
+do{
+    sum+=num
+    num=parseInt(prompt('Введите число'));
+}while (num>0)
+console.log('Сумма-'+sum)
